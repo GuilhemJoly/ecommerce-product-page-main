5a25eaf10804f463648c3903a2d7ba13fa6f6d88
Guilhem27 <guilhem.joly@outlook.fr>
2024-10-23 22:52:06 +0200

    feat: arrangement of elements

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,33 +1,36 @@
 import { Link } from "react-router-dom";
+import './Header.scss';
 
 const Header = () => {
   const avatar = "public/images/image-avatar.png";
   const cartPicture = "public/images/icon-cart.svg";
   return (
-    <>
-      <h2>sneakers</h2>
-      <ul>
-        <li>
-          <Link to={"#"}>Collections</Link>
-        </li>
-        <li>
-          <Link to={"#"}>Men</Link>
-        </li>
-        <li>
-          <Link to={"#"}>Women</Link>
-        </li>
-        <li>
-          <Link to={"#"}>About</Link>
-        </li>
-        <li>
-          <Link to={"#"}>Contact</Link>
-        </li>
-      </ul>
-      <div>
+    <div className="header">
+      <nav>
+        <h2>sneakers</h2>
+        <ul>
+            <li>
+            <Link to={"#"}>Collections</Link>
+            </li>
+            <li>
+            <Link to={"#"}>Men</Link>
+            </li>
+            <li>
+            <Link to={"#"}>Women</Link>
+            </li>
+            <li>
+            <Link to={"#"}>About</Link>
+            </li>
+            <li>
+            <Link to={"#"}>Contact</Link>
+            </li>
+        </ul>
+      </nav>
+      <div className="cartAndAvatar">
         <img src={cartPicture} alt="cart" className="cart"></img>
         <img src={avatar} alt="Avatar" className="avatar" />
       </div>
-    </>
+    </div>
   );
 };
 
