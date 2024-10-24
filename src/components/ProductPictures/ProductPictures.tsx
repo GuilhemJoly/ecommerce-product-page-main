c2232ffd290a9ec12cc7f14a2ac83d88cd0df94d
Guilhem27 <guilhem.joly@outlook.fr>
2024-10-24 17:21:13 +0200

    feat: creating product pictures scss

--- src/components/ProductPictures/ProductPictures.tsx
+++ src/components/ProductPictures/ProductPictures.tsx
@@ -1,14 +1,18 @@
 import { sneakersDatas } from "../../datas/sneakersDatas";
 
+import "./ProductPicture.scss";
+
 export const ProductPictures = () => {
-    const images = sneakersDatas[0].images;
-    return (
-        <div>
-            <img src={images[0]} alt="product big view" />
-            <img src={images[1]} alt="product view 1" />
-            <img src={images[3]} alt="product view 2" /> 
-            <img src={images[5]} alt="product view 3" />
-            <img src={images[7]} alt="product view 4" />
-        </div>
-    );
-}
+  const images = sneakersDatas[0].images;
+  return (
+    <div className="productPictures">
+      <img className="productBigPicture" src={images[0]} alt="product big view" />
+      <div className="productSmallPictures">
+        <img src={images[1]} alt="product view 1" />
+        <img src={images[3]} alt="product view 2" />
+        <img src={images[5]} alt="product view 3" />
+        <img src={images[7]} alt="product view 4" />
+      </div>
+    </div>
+  );
+};
