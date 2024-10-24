3bbe3e67bdc84df40b9692faca1dc4dbd80b7b85
Guilhem27 <guilhem.joly@outlook.fr>
2024-10-24 16:04:36 +0200

    feat: adding datas file with elements of fall limited sneakers

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,6 @@ function App() {
   return (
     <>
       <Header/>
-
-      Sneaker Company
-
-      Fall Limited Edition Sneakers
-
-      These low-profile sneakers are your perfect casual wear companion. Featuring a 
-      durable rubber outer sole, they’ll withstand everything the weather can offer.
-
-      $125.00
-      50%
-      $250.00
-
-      0
-      Add to cart
-      
       <Footer/>
     </>
   )
