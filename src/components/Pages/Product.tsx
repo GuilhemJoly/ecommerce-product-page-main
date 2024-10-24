58eeef73f048ef9ea4e89402737ac2b60eeef917
Guilhem27 <guilhem.joly@outlook.fr>
2024-10-24 16:21:28 +0200

    feat: adding text elements

--- src/components/Pages/Product.tsx
+++ src/components/Pages/Product.tsx
@@ -1,8 +1,20 @@
+import { sneakersDatas } from "../../datas/sneakersDatas";
+
 const Product = () => {
-  return (
-    <div>
-      <h1>Product</h1>
-    </div>
-  );
+    const { brand, name, description, price, discount, previousPrice } = sneakersDatas[0];
+
+    return (
+        <div>
+            <h2>{brand}</h2>
+            <h1>{name}</h1>
+            <p>{description}</p>
+            <div>
+                <span>{price}</span>
+                <span>{discount}</span>
+                <span>{previousPrice}</span>
+            </div>
+        </div>
+    );
 };
+
 export default Product;
