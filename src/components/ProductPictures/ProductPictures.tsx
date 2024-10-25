3cca56f1ac2ad0f52394a9be65cde9a964065dad
Guilhem27 <guilhem.joly@outlook.fr>
2024-10-25 23:35:18 +0200

    feat: update small img border on click with a span

--- src/components/ProductPictures/ProductPictures.tsx
+++ src/components/ProductPictures/ProductPictures.tsx
@@ -14,12 +14,56 @@ export const ProductPictures = () => {
   };
   return (
     <div className="productPictures">
-      <img className="productBigPicture" src={selectedImage} alt="product big view" />
+      <img
+        className="productBigPicture"
+        src={selectedImage}
+        alt="product big view"
+      />
       <div className="productSmallPictures">
-        <img className={selectedIndex === 0 ? "selected" : ""} src={images[1]} alt="product view 1" onClick={() => handleThumbnailClick(images[0],0)}/>
-        <img className={selectedIndex === 2 ? "selected" : ""} src={images[3]} alt="product view 2" onClick={() => handleThumbnailClick(images[2],2)}/>
-        <img className={selectedIndex === 4 ? "selected" : ""} src={images[5]} alt="product view 3" onClick={() => handleThumbnailClick(images[4],4)}/>
-        <img className={selectedIndex === 6 ? "selected" : ""} src={images[7]} alt="product view 4" onClick={() => handleThumbnailClick(images[6],6)}/>
+        <div
+          className="thumbnail-container"
+          onClick={() => handleThumbnailClick(images[0], 0)}
+        >
+          {selectedIndex === 0 && <span className="borderImg"></span>}
+          <img
+            className={selectedIndex === 0 ? "selected" : ""}
+            src={images[1]}
+            alt="product view 1"
+          />
+        </div>
+        <div
+          className="thumbnail-container"
+          onClick={() => handleThumbnailClick(images[2], 2)}
+        >
+          {selectedIndex === 2 && <span className="borderImg"></span>}
+          <img
+            className={selectedIndex === 2 ? "selected" : ""}
+            src={images[3]}
+            alt="product view 2"
+          />
+        </div>
+        <div
+          className="thumbnail-container"
+          onClick={() => handleThumbnailClick(images[4], 4)}
+        >
+          {selectedIndex === 4 && <span className="borderImg"></span>}
+          <img
+            className={selectedIndex === 4 ? "selected" : ""}
+            src={images[5]}
+            alt="product view 3"
+          />
+        </div>
+        <div
+          className="thumbnail-container"
+          onClick={() => handleThumbnailClick(images[6], 6)}
+        >
+          {selectedIndex === 6 && <span className="borderImg"></span>}
+          <img
+            className={selectedIndex === 6 ? "selected" : ""}
+            src={images[7]}
+            alt="product view 4"
+          />
+        </div>
       </div>
     </div>
   );
