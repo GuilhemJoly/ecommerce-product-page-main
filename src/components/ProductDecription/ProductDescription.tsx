f7ac1ec5bcaa1a7323814f7410d45be8a5a2e4b9
Guilhem27 <guilhem.joly@outlook.fr>
2024-10-25 17:41:21 +0200

    feat: adding prices and discount size

--- src/components/ProductDecription/ProductDescription.tsx
+++ src/components/ProductDecription/ProductDescription.tsx
@@ -1,19 +1,24 @@
 import { sneakersDatas } from "../../datas/sneakersDatas";
 
-import './ProductDescription.scss';
+import "./ProductDescription.scss";
 
 export const ProductDescription = () => {
-    const { brand, name, description, price, discount, previousPrice } = sneakersDatas[0];
-    return (
-        <div className="productDescription">
-            <h2 className="productBrand">{brand}</h2>
-            <h1 className="prductName">{name}</h1>
-            <p className="productParagraphDescription">{description}</p>
-            <div className="productPriceElements">
-                <span className="productPrice">{price}</span>
-                <span className="productDiscount">{discount}</span>
-                <span className="productPreviousPrice">{previousPrice}</span>
-            </div>
+  const { brand, name, description, price, discount, previousPrice } =
+    sneakersDatas[0];
+  return (
+    <div className="productDescription">
+      <h2 className="productBrand">{brand}</h2>
+      <h1 className="prductName">{name}</h1>
+      <p className="productParagraphDescription">{description}</p>
+      <div className="productPriceElements">
+        <div className="productPriceAndDiscount">
+          <span className="productPrice">{"$" + price.toFixed(2)}</span>
+          <span className="productDiscount">{discount + "%"}</span>
         </div>
-    );
+        <span className="productPreviousPrice">
+          {"$" + previousPrice.toFixed(2)}
+        </span>
+      </div>
+    </div>
+  );
 };
