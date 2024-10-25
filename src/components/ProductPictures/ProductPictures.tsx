68112ebbcc6202a9e5934c4cab42a41112567081
Guilhem27 <guilhem.joly@outlook.fr>
2024-10-25 21:48:13 +0200

    feat: small img opens large img

--- src/components/ProductPictures/ProductPictures.tsx
+++ src/components/ProductPictures/ProductPictures.tsx
@@ -1,17 +1,23 @@
+import { useState } from "react";
 import { sneakersDatas } from "../../datas/sneakersDatas";
 
 import "./ProductPicture.scss";
 
 export const ProductPictures = () => {
   const images = sneakersDatas[0].images;
+  const [selectedImage, setSelectedImage] = useState(images[0]);
+
+  const handleThumbnailClick = (image: string) => {
+    setSelectedImage(image);
+  };
   return (
     <div className="productPictures">
-      <img className="productBigPicture" src={images[0]} alt="product big view" />
+      <img className="productBigPicture" src={selectedImage} alt="product big view" />
       <div className="productSmallPictures">
-        <img src={images[1]} alt="product view 1" />
-        <img src={images[3]} alt="product view 2" />
-        <img src={images[5]} alt="product view 3" />
-        <img src={images[7]} alt="product view 4" />
+        <img src={images[1]} alt="product view 1" onClick={() => handleThumbnailClick(images[0])}/>
+        <img src={images[3]} alt="product view 2" onClick={() => handleThumbnailClick(images[2])}/>
+        <img src={images[5]} alt="product view 3" onClick={() => handleThumbnailClick(images[4])}/>
+        <img src={images[7]} alt="product view 4" onClick={() => handleThumbnailClick(images[6])}/>
       </div>
     </div>
   );
