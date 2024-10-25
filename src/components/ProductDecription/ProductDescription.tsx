f5d368b8ff88707984d18c6fb3bab61e2e360caf
Guilhem27 <guilhem.joly@outlook.fr>
2024-10-25 16:12:24 +0200

    feat: adding colors and size to product text description

--- src/components/ProductDecription/ProductDescription.tsx
+++ src/components/ProductDecription/ProductDescription.tsx
@@ -1,12 +1,14 @@
 import { sneakersDatas } from "../../datas/sneakersDatas";
 
+import './ProductDescription.scss';
+
 export const ProductDescription = () => {
     const { brand, name, description, price, discount, previousPrice } = sneakersDatas[0];
     return (
-        <div>
+        <div className="productDescription">
             <h2 className="productBrand">{brand}</h2>
             <h1 className="prductName">{name}</h1>
-            <p className="productDescription">{description}</p>
+            <p className="productParagraphDescription">{description}</p>
             <div className="productPriceElements">
                 <span className="productPrice">{price}</span>
                 <span className="productDiscount">{discount}</span>
