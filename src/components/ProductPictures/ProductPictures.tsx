039de06f0c44550cda7be8f97a888cf7ee546451
Guilhem27 <guilhem.joly@outlook.fr>
2024-10-25 22:14:26 +0200

    feat: adding orange border when clicking on small picture

--- src/components/ProductPictures/ProductPictures.tsx
+++ src/components/ProductPictures/ProductPictures.tsx
@@ -6,18 +6,20 @@ import "./ProductPicture.scss";
 export const ProductPictures = () => {
   const images = sneakersDatas[0].images;
   const [selectedImage, setSelectedImage] = useState(images[0]);
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
-  const handleThumbnailClick = (image: string) => {
+  const handleThumbnailClick = (image: string, index: number) => {
     setSelectedImage(image);
+    setSelectedIndex(index);
   };
   return (
     <div className="productPictures">
       <img className="productBigPicture" src={selectedImage} alt="product big view" />
       <div className="productSmallPictures">
-        <img src={images[1]} alt="product view 1" onClick={() => handleThumbnailClick(images[0])}/>
-        <img src={images[3]} alt="product view 2" onClick={() => handleThumbnailClick(images[2])}/>
-        <img src={images[5]} alt="product view 3" onClick={() => handleThumbnailClick(images[4])}/>
-        <img src={images[7]} alt="product view 4" onClick={() => handleThumbnailClick(images[6])}/>
+        <img className={selectedIndex === 0 ? "selected" : ""} src={images[1]} alt="product view 1" onClick={() => handleThumbnailClick(images[0],0)}/>
+        <img className={selectedIndex === 2 ? "selected" : ""} src={images[3]} alt="product view 2" onClick={() => handleThumbnailClick(images[2],2)}/>
+        <img className={selectedIndex === 4 ? "selected" : ""} src={images[5]} alt="product view 3" onClick={() => handleThumbnailClick(images[4],4)}/>
+        <img className={selectedIndex === 6 ? "selected" : ""} src={images[7]} alt="product view 4" onClick={() => handleThumbnailClick(images[6],6)}/>
       </div>
     </div>
   );
