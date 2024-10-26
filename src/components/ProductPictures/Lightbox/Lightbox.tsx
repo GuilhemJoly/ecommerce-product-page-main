a018a64de2ece59183c0dc4d39a0578a8920083b
Guilhem27 <guilhem.joly@outlook.fr>
2024-10-26 23:27:53 +0200

    feat: adding SmallPicture to LightBox

--- src/components/ProductPictures/Lightbox/Lightbox.tsx
+++ src/components/ProductPictures/Lightbox/Lightbox.tsx
@@ -1,12 +1,27 @@
 import { LightboxProps } from "../../../../typescript/types";
 import BigPicture from "../BigPicture/BigPicture";
-import './Lightbox.scss';
+import SmallPictures from "../SmallPictures/SmallPictures";
+import "./Lightbox.scss";
 
-const Lightbox: React.FC<LightboxProps> = ({ imgBig, image, imgSmall, selectedIndex, onClose }) => {
+const Lightbox: React.FC<LightboxProps> = ({
+  imgBig,
+  image,
+  imgSmall,
+  selectedIndex,
+  onClose,
+}) => {
   return (
     <div className="lightbox">
       <h1>Lightbox</h1>
-      <BigPicture image={image}/>
+      <BigPicture image={image} />
+      <div className="SmallPicturesLightBoxContainer">
+        <SmallPictures
+          imgSmall={imgSmall}
+          imgBig={imgBig}
+          selectedIndex={selectedIndex}
+          onThumbnailClick={() => {}}
+        />
+      </div>
     </div>
   );
 };
