c8d568b6a980f079ad2c501587555cc4aa196d42
Guilhem27 <guilhem.joly@outlook.fr>
2024-10-26 21:19:39 +0200

    feat: refactoring with loop

--- src/components/ProductPictures/ProductPictures.tsx
+++ src/components/ProductPictures/ProductPictures.tsx
@@ -4,8 +4,7 @@ import { sneakersDatas } from "../../datas/sneakersDatas";
 import "./ProductPicture.scss";
 
 export const ProductPictures = () => {
-  const imgBig = sneakersDatas[0].imgBig;
-  const imgSmall = sneakersDatas[0].imgSmall;
+  const { imgBig, imgSmall } = sneakersDatas[0];
   const [selectedImage, setSelectedImage] = useState(imgBig[0]);
   const [selectedIndex, setSelectedIndex] = useState(0);
 
@@ -21,50 +20,20 @@ export const ProductPictures = () => {
         alt="product big view"
       />
       <div className="productSmallPictures">
-        <div
-          className="thumbnail-container"
-          onClick={() => handleThumbnailClick(imgBig[0], 0)}
-        >
-          {selectedIndex === 0 && <span className="borderImg"></span>}
-          <img
-            className={selectedIndex === 0 ? "selected" : ""}
-            src={imgSmall[0]}
-            alt="product view 1"
-          />
-        </div>
-        <div
-          className="thumbnail-container"
-          onClick={() => handleThumbnailClick(imgBig[1], 1)}
-        >
-          {selectedIndex === 1 && <span className="borderImg"></span>}
-          <img
-            className={selectedIndex === 1 ? "selected" : ""}
-            src={imgSmall[1]}
-            alt="product view 2"
-          />
-        </div>
-        <div
-          className="thumbnail-container"
-          onClick={() => handleThumbnailClick(imgBig[2], 2)}
-        >
-          {selectedIndex === 2 && <span className="borderImg"></span>}
-          <img
-            className={selectedIndex === 2 ? "selected" : ""}
-            src={imgSmall[2]}
-            alt="product view 3"
-          />
-        </div>
-        <div
-          className="thumbnail-container"
-          onClick={() => handleThumbnailClick(imgBig[3], 3)}
-        >
-          {selectedIndex === 3 && <span className="borderImg"></span>}
-          <img
-            className={selectedIndex === 3 ? "selected" : ""}
-            src={imgSmall[3]}
-            alt="product view 4"
-          />
-        </div>
+        {imgSmall.map((smallImg, index) => (
+          <div
+            key={index}
+            className="thumbnail-container"
+            onClick={() => handleThumbnailClick(imgBig[index], index)}
+          >
+            {selectedIndex === index && <span className="borderImg"></span>}
+            <img
+              className={selectedIndex === index ? "selected" : ""}
+              src={smallImg}
+              alt={`product view ${index + 1}`}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
