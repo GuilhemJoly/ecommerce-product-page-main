18f8c25a42704cafc55f76638202bd162ae86d29
Guilhem27 <guilhem.joly@outlook.fr>
2024-10-28 16:08:12 +0100

    feat: adding static cart design

--- src/components/Cart/CartDropdown.tsx
+++ src/components/Cart/CartDropdown.tsx
@@ -1,23 +1,29 @@
 import Button from "../ui/Button";
-import './CartDropDown.scss'
+import "./CartDropDown.scss";
 
 const CartDropdown = () => {
-    return (
-        <div className="cartDropDown">
-            <h4>Cart</h4>
-            <div className="cartItems">
-                <div className="cartItemDescription">
-                <img src="public/images/fall-limited-edition-sneakers/image-product-1-thumbnail.jpg" />
-                    <span>Product name</span>
-                    <div className="cartPriceSection">
-                        <span>Price</span>
-                        <span>Quantity</span>
-                        <span>Total price</span>
-                    </div>
-                </div>
-                <Button buttonName="Checkout" buttonClass="cartButton"/>
+  return (
+    <div className="cartDropDown">
+      <h4>Cart</h4>
+      <div className="cartItems">
+        <div className="cartItem">
+          <img className="cartImg" src="public/images/fall-limited-edition-sneakers/image-product-1-thumbnail.jpg" />
+          <div className="cartItemDescription">
+            <span>Fall Limited Edition Sneakers</span>
+            <div className="cartPriceSection">
+              <span>$125.00</span>
+              <span>x{" "}</span>
+              <span>3</span>
+              <span className="bold">$375.00</span>
+            </div>
+          </div>
+            <div className="deleteIconContainer">
+                <img className="deleteIcon" src="public\images\icon-delete.svg"/>
             </div>
         </div>
-    )
-}
+        <Button buttonName="Checkout" buttonClass="cartButton" />
+      </div>
+    </div>
+  );
+};
 export default CartDropdown;
