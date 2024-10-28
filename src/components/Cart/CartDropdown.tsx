a20ef632577b7e2290c06533e62ad791abb04702
Guilhem27 <guilhem.joly@outlook.fr>
2024-10-28 20:04:46 +0100

    feat: adding delete from cart

--- src/components/Cart/CartDropdown.tsx
+++ src/components/Cart/CartDropdown.tsx
@@ -1,22 +1,31 @@
+import { useState } from "react";
 import { CartDropdownProps } from "../../../typescript/types";
 import { useLocalStorage } from "../../customHooks/useLocalStorage";
 import Button from "../ui/Button";
 import "./CartDropDown.scss";
 
 const CartDropdown: React.FC<CartDropdownProps> = ({ isOpen}) => {
-    const {handleGetElementFromStorage} = useLocalStorage();
-    const cartItem = handleGetElementFromStorage();
+    const [emptyCart, setEmptyCart] = useState(false);
+    
+    const {handleGetElementFromStorage, handleRemoveFromStorage} = useLocalStorage();
+    let cartItem = handleGetElementFromStorage();
     const name = cartItem?.b?.name || "Error";
     const price = cartItem?.c?.price || 0;
     const quantity = cartItem?.d?.quantity || 0;
     const imgProduct = cartItem?.e?.imgSmall[0] || "";
 
     const finalPrice = price * quantity;
+
+    const handleDeleteCart = () => {
+        handleRemoveFromStorage();
+        cartItem = [...[]];
+        setEmptyCart(true);
+    };
     
   return (
     <div className={`cartDropDown ${!isOpen ? "hidden" : ""}`}>
       <h4>Cart</h4>
-      {!cartItem ? (
+      {(!cartItem || emptyCart) ? (
         <div className="isEmptyDiv">
             <span className="isEmptySpan">Your cart is empty</span>
         </div>
@@ -37,7 +46,7 @@ const CartDropdown: React.FC<CartDropdownProps> = ({ isOpen}) => {
               </div>
             </div>
             <div className="deleteIconContainer">
-              <img className="deleteIcon" src="public\images\icon-delete.svg" />
+              <img className="deleteIcon" src="public\images\icon-delete.svg" onClick={handleDeleteCart }/>
             </div>
           </div>
           <Button buttonName="Checkout" buttonClass="cartButton" />
