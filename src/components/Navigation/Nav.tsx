881e25a1e63d9511e307d4a3553e9f3840d8f652
Guilhem27 <guilhem.joly@outlook.fr>
2024-10-31 18:37:49 +0100

    feat: add burger menu to open nav below 768px

--- src/components/Navigation/Nav.tsx
+++ src/components/Navigation/Nav.tsx
@@ -27,18 +27,28 @@ const NavDesktop = () => {
   }, []);
 
   return (
-    <nav className={isNavOpen}>
+    <nav>
       <img src="public/images/logo.svg" alt="logo" className="logo" />
       <div className="closeIcon" onClick={toggleNav}>
-        <svg width="14" height="15" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
-            fill=""
-            fillRule="evenodd"
-          />
-        </svg>
+        {isNavOpen === "hidden" ? (
+          <svg width="16" height="15" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M16 12v3H0v-3h16Zm0-6v3H0V6h16Zm0-6v3H0V0h16Z"
+              fill="#69707D"
+              fill-rule="evenodd"
+            />
+          </svg>
+        ) : (
+          <svg width="14" height="15" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
+              fill=""
+              fillRule="evenodd"
+            />
+          </svg>
+        )}
       </div>
-      <ul>
+      <ul className={isNavOpen}>
         <li>
           <Link to={"#"}>Collections</Link>
         </li>
