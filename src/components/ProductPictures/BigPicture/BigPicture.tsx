b96ef349bd8e5333888d2b5992d0c3f8cddaa382
Guilhem27 <guilhem.joly@outlook.fr>
2024-11-01 20:13:05 +0100

    feat: adding static arrow button without arrow size

--- src/components/ProductPictures/BigPicture/BigPicture.tsx
+++ src/components/ProductPictures/BigPicture/BigPicture.tsx
@@ -1,14 +1,23 @@
 import { BigPictureProps } from '../../../../typescript/types';
+import { useImageNavigation } from '../../../customHooks/useImageNavigation';
+import ArrowButton from '../../ui/ArrowButton';
 import './BigPicture.scss';
 
 const BigPicture: React.FC<BigPictureProps> = ({ image, onClick }) => {
+    const {handlePrevClick, handleNextClick } = useImageNavigation();
     return (
+        <div className="bigPicture">
         <img
             className="productBigPicture"
             src={image}
             alt="product big view"
             onClick={onClick}
         />
+        <div className="arrowButtonSection">
+        <ArrowButton isLeftButton={true} imgChange={handlePrevClick} />
+        <ArrowButton isLeftButton={false} imgChange={handleNextClick} />
+        </div>
+        </div>
     );
 }
 
