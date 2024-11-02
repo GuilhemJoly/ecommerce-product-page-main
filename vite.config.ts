0e218b59ab21e369cfc764da777d49bde9222f8a
Guilhem27 <guilhem.joly@outlook.fr>
2024-11-02 22:50:26 +0100

    refactor: add scss imports into vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/Scss-variables/_colors.scss";
+                        @import "./src/Scss-variables/_mixins.scss";
+                        @import "./src/Scss-variables/_typography.scss";`,
+      },
+    },
+  },
+});
