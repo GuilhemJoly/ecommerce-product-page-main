d53f6cddc0270fd40cab51d552254babe41af47f
Guilhem27 <guilhem.joly@outlook.fr>
2024-11-04 18:44:32 +0100

    refactor: add datas file

---
+++ src/datas/navLinkDatas.ts
@@ -0,0 +1,19 @@
+export const navLinkDatas = [
+    {
+        title: "Collections",
+        link: "#",
+    },
+    { 
+        title: "Men",
+        link: "#",
+    },
+    {   title: "Women",
+        link: "#",
+    },
+    {   title: "About",
+        link: "#",
+    },
+    {   title: "Contact",
+        link: "#",
+    },
+];
