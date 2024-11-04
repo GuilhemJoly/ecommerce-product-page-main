ccd7ec928cb41e72b2264a7026fd3c6570cc19d1
Guilhem27 <guilhem.joly@outlook.fr>
2024-11-04 20:52:03 +0100

    feat: format code in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import './App.scss'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import { Route, Routes } from 'react-router-dom'
-import { navLinkDatas } from './datas/navLinkDatas'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Route, Routes } from "react-router-dom";
+import { navLinkDatas } from "./datas/navLinkDatas";
+import "./App.scss";
 
 function App() {
-
   return (
     <>
-      <Header/>
+      <Header />
       <Routes>
         {navLinkDatas.map((navLink) => (
-          <Route key={navLink.id} path={navLink.link} element={<navLink.element />} />
+          <Route
+            key={navLink.id}
+            path={navLink.link}
+            element={<navLink.element />}
+          />
         ))}
       </Routes>
-      <Footer/>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
