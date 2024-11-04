4c9da7781725f6e0181479afa11de09bb1ffad04
Guilhem27 <guilhem.joly@outlook.fr>
2024-11-04 20:59:29 +0100

    feat: add doc to navLinkDatas

--- src/datas/navLinkDatas.ts
+++ src/datas/navLinkDatas.ts
@@ -4,37 +4,37 @@ export const navLinkDatas = [
     {
         id: 1,
         title: "Home",
-        link: "product",
-        element: Product,
+        link: "product", // Change it to "home".
+        element: Product, // Product is a placeholder, change it to the Home component.
     },
     {
         id: 2,
         title: "Collections",
         link: "/collections",
-        element: Product,
+        element: Product, // Product is a placeholder, change it to the Collections component.
     },
     { 
         id: 3,
         title: "Men",
         link: "/men",
-        element: Product,
+        element: Product, // Product is a placeholder, change it to the Men component.
     },
     {   
         id: 4,
         title: "Women",
         link: "/women",
-        element: Product,
+        element: Product, // Product is a placeholder, change it to the Women component.
     },
     {  
         id: 5,
         title: "About",
         link: "/about",
-        element: Product,
+        element: Product, // Product is a placeholder, change it to the About component.
     },
     {  
         id: 6,
         title: "Contact",
         link: "/contact",
-        element: Product,
+        element: Product, // Product is a placeholder, change it to the Contact component.
     },
 ];
