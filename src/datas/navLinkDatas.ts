419205d9f2b780e4529850c22f448f088b52ad83
Guilhem27 <guilhem.joly@outlook.fr>
2024-11-05 17:06:23 +0100

    update nav and final screenshots

--- src/datas/navLinkDatas.ts
+++ src/datas/navLinkDatas.ts
@@ -1,38 +1,32 @@
 import Product from '@components/Pages/Product';
 
 export const navLinkDatas = [
-    {
+  {
         id: 1,
-        title: "Home",
-        link: "/", 
-        element: Product, // Product is a placeholder, change it to the Home component.
-    },
-    {
-        id: 2,
         title: "Collections",
-        link: "/collections",
+        link: "/",
         element: Product, // Product is a placeholder, change it to the Collections component.
     },
     { 
-        id: 3,
+        id: 2,
         title: "Men",
         link: "/men",
         element: Product, // Product is a placeholder, change it to the Men component.
     },
     {   
-        id: 4,
+        id: 3,
         title: "Women",
         link: "/women",
         element: Product, // Product is a placeholder, change it to the Women component.
     },
     {  
-        id: 5,
+        id: 4,
         title: "About",
         link: "/about",
         element: Product, // Product is a placeholder, change it to the About component.
     },
     {  
-        id: 6,
+        id: 5,
         title: "Contact",
         link: "/contact",
         element: Product, // Product is a placeholder, change it to the Contact component.
